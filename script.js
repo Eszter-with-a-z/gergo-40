2eaa8c0287c321a9ae1a12017393c2ea0e400ca9
Eszter-with-a-z <85640262+Eszter-with-a-z@users.noreply.github.com>
2024-11-17 01:54:42 +0800

    Upload initial version
    first 10 sources

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+  alert(`Kedves Gergő! Boldog születésnapot! 
+    40. születésnapod alkalmából összegyűjtöttünk néhány dokumentumfilmet, interjút, beszédet, és mindenfélét, amiről úgy gondoltuk, érdekes lehet számodra. 
+    
+    Reméljük, hogy sok új gondolattal gazdagodsz, és hogy ezeket majd velünk is megosztod!
+    Jó böngészést!`)
+});
